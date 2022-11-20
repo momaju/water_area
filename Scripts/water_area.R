75c0fe4564ceac958094244b5073595dacdd2c52
momaju <92119184+momaju@users.noreply.github.com>
2022-11-20 09:44:19 -0300

    Initial commit

---
+++ Scripts/water_area.R
@@ -0,0 +1,44 @@
+library(tidyverse)
+library(janitor)
+library(stringr)
+
+
+#South America only
+
+inland_waters_sa <- read_csv("raw/inland_waters11-14-2022.csv") %>% 
+  clean_names() %>% 
+  filter(area_code %in% c(9,19,21,40,44,58,169,170,91, 236))
+
+inland_2019 <- inland_waters_sa %>% 
+  filter(year == 2019)
+  
+
+
+
+# Renaming Bolívia and Venezuela --------------------------------------------------------
+
+
+
+inland_waters_sa["area"][inland_waters_sa["area"] == 
+"Bolivia (Plurinational State of)"] <- "Bolivia"
+
+inland_waters_sa["area"][inland_waters_sa["area"] == 
+"Venezuela (Bolivarian Republic of)"] <- "Venezuela"
+
+
+# Produção de pescado -----------------------------------------------------
+
+
+
+quantity_fw <- read_csv("raw/aquaculture_quantity_fw.csv")
+
+
+
+
+
+
+
+
+
+
+
